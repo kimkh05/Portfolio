ef652d7fa0cb97ac101bd9c1938dae9c605f9d0c
kimkh05 <nestjs01@naver.com>
2022-09-23 19:51:10 +0900

    feat: title 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import { Footer } from "../components/footer/Footer";
 import { Wrapper } from "../components/header/Wrapper";
+import { Introduce } from "../components/Introduce";
+import { Project } from "../components/Project";
 
 const Home: NextPage = () => {
   return (
     <>
       <Wrapper>
-        <span>PropsType</span>
+        <Head>
+          <title>김경호의 포트폴리오</title>
+        </Head>
+        <Introduce />
+        <Project />
+        <Footer />
       </Wrapper>
     </>
   );
