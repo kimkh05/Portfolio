c876aa906427d8e3581e2a367b03e2be6377fc2c
kimkh05 <nestjs01@naver.com>
2022-09-23 16:23:39 +0900

    feat: 더미데이터 추가

---
+++ constance/Project.ts
@@ -0,0 +1,22 @@
+interface DummyType {
+  name: string;
+  time: string;
+  job: string[];
+  introduce: string;
+}
+
+export const Project: DummyType[] = [
+  {
+    name: "DSM-MM",
+    time: "2022.05.13 ~ ",
+    job: ["Frontend", "Design"],
+    introduce:
+      "멘토링 사이트와 프로젝트 사이트를 함으로 많은 것을 배울 수 있습니다.",
+  },
+  {
+    name: "Maradi",
+    time: "2022.09.01 ~ ",
+    job: ["Frontend", "Design"],
+    introduce: "프로젝트에 펀딩을 하여 다른사람에게 도움을 줄 수 있어요",
+  },
+];
