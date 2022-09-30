40d74423990ee6a602446196d1bdc93a187f9af6
kimkh05 <nestjs01@naver.com>
2022-09-30 11:27:13 +0900

    feat: 더미데이터 추가

--- constance/Project.ts
+++ constance/Project.ts
@@ -2,7 +2,6 @@ interface DummyType {
   id: number;
   name: string;
   time: string;
-  job: string[];
   introduce: string;
   develop: string[];
 }
@@ -12,16 +11,14 @@ export const ProjectDummy: DummyType[] = [
     id: 1,
     name: "DSM-MM",
     time: "2022.05.13 ~ ",
-    job: ["Frontend", "Design"],
     introduce:
       "멘토링 사이트와 프로젝트 사이트를 함으로 많은 것을 배울 수 있습니다.",
-    develop: ["React", "TypeScript", "Emotion"],
+    develop: ["React", "TypeScript", "Emotion.js"],
   },
   {
     id: 2,
     name: "Maradi",
     time: "2022.09.01 ~ ",
-    job: ["Frontend", "Design"],
     introduce: "프로젝트에 펀딩을 하여 다른사람에게 도움을 줄 수 있어요",
     develop: ["React", "TypeScript", "Styled-Component"],
   },
