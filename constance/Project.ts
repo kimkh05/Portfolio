9b76fb285b553ce86c0e9f9503f5f335422235af
kimkh05 <nestjs01@naver.com>
2022-10-02 14:12:46 +0900

    feat: 더미데이터 값 추가

--- constance/Project.ts
+++ constance/Project.ts
@@ -22,4 +22,11 @@ export const ProjectDummy: DummyType[] = [
     introduce: "프로젝트에 펀딩을 하여 다른사람에게 도움을 줄 수 있어요",
     develop: ["React", "TypeScript", "Styled-Component"],
   },
+  {
+    id: 3,
+    name: "Portfolio",
+    time: "2020.09.22 ~ ",
+    introduce: "저의 스택들을 저만의 디자인으로 보여줄 수 있어요!",
+    develop: ["React", "TypeScript", "NextJs", "Emotion"],
+  },
 ];
