5fee2aa14b4348812cfba89340383120451953a3
kimkh05 <nestjs01@naver.com>
2022-11-29 10:44:29 +0900

    feat: add image

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,5 @@
-export { default as IntroduceImage } from './img/introduce.png';
-export { default as GithubLogo } from './img/github.svg';
-export { default as MainLogo } from './img/main.svg';
+export { default as IntroduceImage } from "./img/introduce.png";
+export { default as GithubLogo } from "./img/github.svg";
+export { default as MainLogo } from "./img/main.svg";
+export { default as MoveScrollDown } from "./img/move_scroll_down.svg";
+export { default as MoveScrollUp } from "./img/move_scroll_up.svg";
