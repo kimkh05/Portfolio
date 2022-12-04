1b40abf1beb1b16ae35096d4278e26ceed66b46c
kimkh05 <nestjs01@naver.com>
2022-12-04 23:18:33 +0900

    feat: 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export default function custom404() {
+  const nav = useRouter();
+  return (
+    <_Wrapper>
+      <span>뒤로 가기</span>
+    </_Wrapper>
+  );
+}
+
+const _Wrapper = styled.div``;
